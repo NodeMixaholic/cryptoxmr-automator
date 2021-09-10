92e6e9259bfd70180333b18eb533297eaf57e785
Samuel Lord <sam@sllord.info>
2021-09-10 03:01:04 +0000

    make not headless

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const puppeteer = require('puppeteer')
 
 async function openBrowserAndRun() {
-    const browser = await puppeteer.launch({headless: true})
+    const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
     await page.goto("https://cryptoxmr.net/home")
     try {
