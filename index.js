662b4e2cbe7302aba910f06f65224c678eb8bea2
Samuel Lord <sam@sllord.info>
2021-09-12 21:35:00 -0500

    make headless again for better ux

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const puppeteer = require('puppeteer')
 
 async function openBrowserAndRun() {
-    const browser = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({headless: true})
     const page = await browser.newPage()
     await page.goto("https://cryptoxmr.net/home")
     try {
