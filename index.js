b350ca7d735d8e19c60b62e849e2b12c23c12fc5
Samuel Lord <sam@sllord.info>
2021-09-14 04:27:54 +0000

    lol thats why its constantly refreshing
    lol i am dumb xD

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ async function openBrowserAndRun() {
         console.log("error in my code lol")
     }
     console.log(`As long as this is open, collecting XMR.`)
+    page.setDefaultNavigationTimeout(9999999);
+    await page.waitForNavigation().catch(err => process.exit(404)); //hacky fix lol
     while (true) {
         await new Promise(resolve => setTimeout(resolve, 500)).catch(err => console.log(err)); //just in case ;)
-        page.setDefaultNavigationTimeout(9999999);
-        await page.waitForNavigation().catch(err => process.exit(404)); //hacky fix lol
         try {
             const c = await page.evaluate(() => {
                 async function autoxmr() {
